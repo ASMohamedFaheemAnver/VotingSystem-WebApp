ab8868814aeaf5316718d7639446ae41288ac031
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2020-11-09 21:58:31 +0530

    Seeder,

--- seeder/seeder.js
+++ seeder/seeder.js
@@ -3,83 +3,114 @@ import ApolloClient, { gql } from "apollo-boost";
 const fs = require("fs");
 
 const client = new ApolloClient({
-  // uri: "https://freedom-voting-system.herokuapp.com/",
-  uri: "http://localhost:4000/",
+  uri: "https://freedom-voting-system.herokuapp.com/",
+  // uri: "http://localhost:4000/",
   headers: {
     Authorization: process.env.Authorization,
   },
 });
 
-// fs.readFile("members-fetched.json", "utf8", async function (err, data) {
-//   if (err) {
-//     console.log(err.message);
-//   }
-//   const members = JSON.parse(data);
-//   console.log({ members: members });
-//   console.log({ membersLength: members.length });
+function seedMembers() {
+  fs.readFile("members-fetched.json", "utf8", async function (err, data) {
+    if (err) {
+      console.log(err.message);
+    }
+    const members = JSON.parse(data);
+    console.log({ members: members });
+    console.log({ membersLength: members.length });
 
-//   // for (let i = 0; i < members.length; i++) {
-//   //   const createMember = gql`
-//   //     mutation {
-//   //       createMember(data: { name: "${members[i].name}", year: ${members[i].year}, gender: "${members[i].gender}" }) {
-//   //         _id
-//   //       }
-//   //     }
-//   //   `;
+    for (let i = 0; i < members.length; i++) {
+      const createMember = gql`
+      mutation {
+        createMember(data: { name: "${members[i].name}", year: ${members[i].year}, gender: "${members[i].gender}" }) {
+          _id
+        }
+      }
+    `;
 
-//   //   const member = await client.mutate({ mutation: createMember });
-//   //   console.log({
-//   //     numberOfMembersCreated: i,
-//   //     member: member.data.createMember._id,
-//   //   });
-//   // }
+      const member = await client.mutate({ mutation: createMember });
+      console.log({
+        numberOfMembersCreated: i,
+        member: member.data.createMember._id,
+      });
+    }
 
-//   console.log({ msg: "Done." });
-// });
+    console.log({ msg: "Done." });
+  });
+}
 
-// const getAllMembers = gql`
-//   query {
-//     getAllMembers {
-//       secret
-//       year
-//       gender
-//       name
-//     }
-//   }
-// `;
+function getAllMembers() {
+  const getAllMembers = gql`
+  query {
+    getAllMembers {
+      secret
+      year
+      gender
+      name
+    }
+  }
+`;
 
-// client.query({ query: getAllMembers }).then((res) => {
-//   fs.writeFile(
-//     "members-fetched.json",
-//     JSON.stringify(res.data.getAllMembers),
-//     "utf8",
-//     (err) => {
-//       if (err) {
-//         console.log(err);
-//       }
-//     }
-//   );
-// });
+  client.query({ query: getAllMembers }).then((res) => {
+    fs.writeFile(
+      "members-fetched.json",
+      JSON.stringify(res.data.getAllMembers),
+      "utf8",
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+  });
+}
 
-// const getAllPositions = gql`
-//   query {
-//     getAllPositions {
-//       title
-//       eligible_year
-//       eligible_gender
-//     }
-//   }
-// `;
+function getAllPositions() {
+  const getAllPositions = gql`
+    query {
+      getAllPositions {
+        title
+        eligible_year
+        eligible_gender
+      }
+    }
+  `;
 
-// client.query({ query: getAllPositions }).then((res) => {
-//   fs.writeFile(
-//     "position-fetched.json",
-//     JSON.stringify(res.data.getAllPositions),
-//     "utf8",
-//     (err) => {
-//       if (err) {
-//         console.log(err);
-//       }
-//     }
-//   );
-// });
+  client.query({ query: getAllPositions }).then((res) => {
+    fs.writeFile(
+      "position-fetched.json",
+      JSON.stringify(res.data.getAllPositions),
+      "utf8",
+      (err) => {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+  });
+}
+
+
+function seedPositions() {
+  fs.readFile("position-fetched.json", "utf8", async (err, data) => {
+    const positions = JSON.parse(data);
+
+    for (let i = 0; i < positions.length; i++) {
+      const createPosition = gql`
+        mutation {
+          createPosition(
+            data: {
+              title: "${positions[i].title}"
+              eligible_year: ${positions[i].eligible_year}
+              eligible_gender: "${positions[i].eligible_gender}"
+            } ) {
+              _id
+            }
+          }
+      `;
+
+      const position = await client.mutate({ mutation: createPosition });
+      console.log({ _id: position.data.createPosition._id })
+    }
+  })
+}
